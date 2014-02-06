543354affea84c8adaf6bd51f0daeafbcc69c391
Liviu Cristian Mirea Ghiban <contact@liviucmg.com>
2014-02-06 19:42:21 +0200

    Fixed postgres temp folder creation.

--- postgres/backup.sh
+++ postgres/backup.sh
@@ -2,10 +2,10 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Backing up all PostgreSQL databases.\n";
+echo -e "Backing up all PostgreSQL databases.\n";
 
-rm -r $DIR/temp
 mkdir $DIR/temp
 
-mkdir $DIR/temp/postgresql 
 sudo -i -u postgres pg_dumpall > $DIR/temp/dumpall.sql
+
+rm -r $DIR/temp
